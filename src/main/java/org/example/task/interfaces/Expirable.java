49f2604eb7218a55d81ad625941a7038dc757cd2
Orangekun <amazingritro66@gmail.com>
2025-07-04 17:22:04 +0300

    Remove comments from Expirable and Shippable interfaces for cleaner code

--- src/main/java/org/example/task/interfaces/Expirable.java
+++ src/main/java/org/example/task/interfaces/Expirable.java
@@ -2,9 +2,8 @@ package org.example.task.interfaces;
 
 import java.util.Date;
 
-// Interface for products with expiration dates
 public interface Expirable {
-    boolean isExpired();       // Check if product has expired
+    boolean isExpired();       
     void setExpirationDate(Date expirationDate);
     Date getExpirationDate();
 }
--- src/main/java/org/example/task/interfaces/Shippable.java
+++ src/main/java/org/example/task/interfaces/Shippable.java
@@ -1,7 +1,7 @@
 package org.example.task.interfaces;
 
-// Interface for products that can be physically shipped
+
 public interface Shippable {
-    String getName();      // Get the product name
-    double getWeight();    // Get weight in kg for shipping calculations
+    String getName();     
+    double getWeight();    
 }
